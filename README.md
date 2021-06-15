4c4ea47fe82f2380424b96d5c3157f15cd887ad7
eiji.ienaga <eiji.ienaga@gmail.com>
2021-06-15 16:48:28 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+## インストール ＆ テスト実行
+
+```
+yarn install
+yarn jest --no-cache --watchAll
+```
+or
+
+```
+npm install
+npm test -- --no-cache --watchAll
+```
---
+++ src/statement.test.js
@@ -0,0 +1,30 @@
+const statement = require('./statement');
+
+test('ステートメントが出力できることその１（本に載っているサンプル例）', () => {
+  const customer = {
+    "name": "martin",
+    "rentals": [
+      {"movieID": "F001", "days": 3},
+      {"movieID": "F002", "days": 1},
+    ]
+  };
+
+  const movies = {
+    "F001": {"title": "Ran",                     "code": "regular"},
+    "F002": {"title": "Trois Couleurs: Bleu",     "code": "regular"},
+    // etc
+  };
+  
+
+  const expected =
+`Rental Record for martin
+  Ran  3.5
+  Trois Couleurs: Bleu  2
+Amount owed is 5.5
+You earned 2 frequent renter points
+`;
+  const result = statement(customer, movies);
+  console.log(result)
+  expect(result).toEqual(expected);
+});
+
