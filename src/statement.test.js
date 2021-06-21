3b7222d2fb4d11758bf28ec792e533e6e938637d
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2021-06-21 11:10:16 +0900

    fix lint error

--- src/statement.test.js
+++ src/statement.test.js
@@ -2,16 +2,16 @@ const statement = require('./statement');
 
 test('ステートメントが出力できることその１（本に載っているサンプル例）', () => {
   const customer = {
-    "name": "martin",
-    "rentals": [
-      {"movieID": "F001", "days": 3},
-      {"movieID": "F002", "days": 1},
+    'name': 'martin',
+    'rentals': [
+      {'movieID': 'F001', 'days': 3},
+      {'movieID': 'F002', 'days': 1},
     ]
   };
 
   const movies = {
-    "F001": {"title": "Ran",                     "code": "regular"},
-    "F002": {"title": "Trois Couleurs: Bleu",     "code": "regular"},
+    'F001': {'title': 'Ran',                     'code': 'regular'},
+    'F002': {'title': 'Trois Couleurs: Bleu',     'code': 'regular'},
     // etc
   };
   
@@ -24,7 +24,7 @@ Amount owed is 5.5
 You earned 2 frequent renter points
 `;
   const result = statement(customer, movies);
-  console.log(result)
+  console.log(result);
   expect(result).toEqual(expected);
 });
 
