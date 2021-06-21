829faf7915bd8d16795bbe7e530468c56628c154
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2021-06-21 12:08:10 +0900

    fix lint

--- src/statement.js
+++ src/statement.js
@@ -8,21 +8,21 @@ function statement(customer, movies) {
 
     // determine amount for each movie
     switch (movie.code) {
-    case 'regular':
-      thisAmount = 2;
-      if (r.days > 2) {
-        thisAmount += (r.days - 2) * 1.5;
-      }
-      break;
-    case 'new':
-      thisAmount = r.days * 3;
-      break;
-    case 'childrens':
-      thisAmount = 1.5;
-      if (r.days > 3) {
-        thisAmount += (r.days - 3) * 1.5;
-      }
-      break;
+      case 'regular':
+        thisAmount = 2;
+        if (r.days > 2) {
+          thisAmount += (r.days - 2) * 1.5;
+        }
+        break;
+      case 'new':
+        thisAmount = r.days * 3;
+        break;
+      case 'childrens':
+        thisAmount = 1.5;
+        if (r.days > 3) {
+          thisAmount += (r.days - 3) * 1.5;
+        }
+        break;
     }
 
     //add frequent renter points
